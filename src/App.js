c4d0e3782a238a3f8165a11ac3ae994dfeae0677
Dave Genest <drwgenest@gmail.com>
2020-01-10 07:49:36 -0800

    Adds theme toggle

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useReducer } from 'react'
 import { Route, Switch } from 'react-router-dom'
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import reducer, { INITIAL_STATE } from './store/reducer'
 import * as actions from './store/actionTypes'
 import Menu from './components/Menu'
@@ -14,14 +16,17 @@ import './App.scss';
 const App = () => {
   const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
   const closeDrawer = () => state.isDrawerOpen ? dispatch({ type: actions.TOGGLE_DRAWER}) : null
+  const toggleTheme = () => dispatch({ type: actions.SET_THEME, theme: state.theme === 'dark' ? 'light' : 'dark'})
 
   return (
     <div className={`app ${state.theme}-theme`}>
       <div className='nav-container'>
+        <FontAwesomeIcon className='nav-icon' icon={state.theme === 'dark' ? faSun : faMoon} onClick={toggleTheme} />
         <Menu menuitems={routes} />
       </div>
       <div className='nav-container-responsive'>
-        <i className='material-icons' onClick={() => dispatch({ type: actions.TOGGLE_DRAWER })}>menu</i>
+        <i className='material-icons pointer' onClick={() => dispatch({ type: actions.TOGGLE_DRAWER })}>menu</i>
+        <FontAwesomeIcon icon={state.theme === 'dark' ? faSun : faMoon} onClick={toggleTheme} />
       </div>
       <div className='nav-drawer' style={{ display: state.isDrawerOpen ? 'block' : 'none' }}>
         <Menu menuitems={routes} clickHandler={closeDrawer} />
